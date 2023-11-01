f7bd161ad49c44808dc1dc5bdadcbb04ef90ba9b
halima2014 <benkhalifahalima@hotmail.com>
2023-11-01 18:45:42 +0100

    config_pdo

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
- 
-    <h1>TEST</h1>
+    <?php  require_once 'templates/header.php';
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
- integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
+    require_once 'lib/poll.php';
+    
+    $polls = getPolls($pdo);
+    
+    ?>
+
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="<?=PATH_ASSETS_IMAGES?>logo-votit.png" class="d-block mx-lg-auto img-fluid" alt="logo site" width="400" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Votez sur des sujets de l'actualité IT</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <a type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir les sondages</a>
+          
+        </div>
+      </div>
+    </div>
+
+    <div class="row text-center">
+        <h2>Les derniers sondages :</h2>
+        <?php foreach($polls as $poll){
+          require 'templates/poll_part.php';
+        } ?>
+        
+        
+    </div>
+        
+    <?php  require_once 'templates/footer.php' ?>
